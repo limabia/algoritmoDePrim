57e0f5e89ea1e9c539818fb2e80ea6ce65edfc3d
limabia <bianca.lima.1504@gmail.com>
2018-04-07 20:00:25 -0300

    arquivos bases para desenvolvimento do trabalho

---
+++ d-10346811.c
@@ -0,0 +1,39 @@
+/* Nome: Bianca Lima Santos  		Nº USP: 10346811 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+o algoritmo recebe um grafo
+G = (V, E) e devolve uma árvore T = (S, E0
+).
+*/
